9d2b45b598b5a343bff1c03c1a41fc55dcc313b1
aljaamiu <yakubumuhammedjaamiu@gmail.com>
2026-06-08 00:03:53 +0800

    account middle name

--- app/Http/Controllers/API/AccountAPIController.php
+++ app/Http/Controllers/API/AccountAPIController.php
@@ -566,15 +566,16 @@ class AccountAPIController extends AppBaseController
                     ->buildCreateAccountPayload([
 
                         // if middle name
-                        'firstname' => $user->first_name.' '.$user->middle_name,
-                        'lastname' => $user->last_name,
-                        'middlename' => $user->middle_name,
+                        // 'firstname' => $user->first_name.' '.$user->middle_name,
+                        // 'lastname' => $user->last_name,
+                        // 'middlename' => $user->middle_name,
                         // 'middlename' => null,
 
 
-                        // 'firstname' => $user->first_name,
-                        // 'lastname' => $user->last_name,
-                        // 'middlename' => $user->middle_name,
+                        // normal
+                        'firstname' => $user->first_name,
+                        'lastname' => $user->last_name,
+                        'middlename' => $user->middle_name,
 
 
                         // 'firstname' => $verifiedFirstName,
